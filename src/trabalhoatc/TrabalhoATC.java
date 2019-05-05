a1f6019eecb9ff2bb081fcdff4913a864da22fea
Mattheus Soares <tz_santos@hotmail.com>
2019-05-04 22:08:03 -0300

    Commit Inicial

---
+++ src/trabalhoatc/TrabalhoATC.java
@@ -0,0 +1,18 @@
+package trabalhoatc;
+
+import java.util.Scanner;
+import trabalhoatc.TagReader;
+import trabalhoatc.FileManager;
+
+public class TrabalhoATC {
+
+    public static void main(String[] args) {
+
+            Scanner sc = new Scanner(System.in);
+            
+            String tag = sc.next();
+            System.out.println(tag);
+            
+    }
+    
+}
