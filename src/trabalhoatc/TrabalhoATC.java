7a6b0da4903597aef818b3d4d1c92c862719dddb
Mattheus Soares <tz_santos@hotmail.com>
2019-05-04 22:43:32 -0300

    Lista do que fazer definida

--- src/trabalhoatc/TrabalhoATC.java
+++ src/trabalhoatc/TrabalhoATC.java
@@ -7,12 +7,37 @@ import trabalhoatc.FileManager;
 public class TrabalhoATC {
 
     public static void main(String[] args) {
-
+        /*
+            String op = new String();
+            While(op!=":q"){
+                ->código abaixo
+                op = tag;
+            }
+        
+        */
             Scanner sc = new Scanner(System.in);
             
             String tag = sc.next();
             System.out.println(tag);
+            /*
+            Fazer tratamento das tags, verificar a digitação
             
+            Realizar as operações das tags de comando: escrita e leitura de 
+            arquivo; salvamento de tags e afins
+            
+            */
+            
+            /*if (tag.equals(":l")){
+                CARREGAR TAGS DO ARQUIVO
+            }*/
+            
+                        
+            String value = sc.next();
+            System.out.println(value);
+            
+            /*Verificar e validar valor de value*/
+                        
+            sc.close();
     }
     
 }
